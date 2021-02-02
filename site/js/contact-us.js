1e78d632c2c7bae3db1b8377a45679e9a30f94bd
Yatin <bhatiayatin09@gmail.com>
2021-02-03 02:31:52 +0530

    adding form js

---
+++ site/js/contact-us.js
@@ -0,0 +1,5 @@
+function validation(){
+	var fname = document.getElementById("fname").value;
+	var lname = document.getElementById("lname").value;
+	alert(fname + " " + lname + ", your details are submitted. We will get back to you!");
+}
