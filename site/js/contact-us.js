cd57084abc44dd285e2ff62ecb7dba6dbc7652a8
Yatin <bhatiayatin09@gmail.com>
2021-02-14 04:00:46 +0530

    completing confirmation page

--- site/js/contact-us.js
+++ site/js/contact-us.js
@@ -1,5 +1,5 @@
 function validation(){
 	const fname = document.getElementById("fname").value;
 	const lname = document.getElementById("lname").value;
-	alert(fname + " " + lname + ", your details are submitted. We will get back to you as soon as possible!");
+	alert(fname + " " + lname + ", your details are submitted.\nWe will get back to you as soon as possible!");
 }
